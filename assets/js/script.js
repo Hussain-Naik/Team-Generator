c1d4e609e558eabe5ba88ee954b4117a6dc6969f
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2023-07-15 22:58:14 +0100

    Add Random sort and save to localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,44 @@ function generatePlayers() {
     let players = document.getElementById('players');
     for (let i = 0 ; i < limit; i++) {
         players.innerHTML += `<label for="player${i+1}">Player ${i+1}</label>
-        <input type="text" id="player${i+1}">`
+        <input type="text" id="player${i+1}" class="players">`;
     }
-    console.log(players.innerHTML);
+    players.innerHTML += `<input type="button" value="Save" onclick="savePlayers()">
+    <input type="button" value="Load" onclick="loadPlayers()">
+    <input type="button" value="Randomize" onclick="randomizePlayers()">`;
+}
 
+function randomizePlayers() {
+    let playersArray = [];
+    for (let i = 0; i < document.getElementsByClassName('players').length ; i++) {
+        playersArray[i] = document.getElementsByClassName('players')[i].value;
+    };
+
+    for (let i = playersArray.length -1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i+1));
+    let k = playersArray[i];
+    playersArray[i] = playersArray[j];
+    playersArray[j] = k;
+    }
+    savePlayers(playersArray);
+    loadPlayers();
+}
+
+function savePlayers(players) {
+    console.log(players);
+    let playersArray = [];
+    for (let i = 0; i < document.getElementsByClassName('players').length ; i++) {
+        playersArray[i] = document.getElementsByClassName('players')[i].value;
+    };
+    players = (typeof(players) != 'undefined') ? players : playersArray;
+    localStorage.setItem('players' , players);
+}
+
+function loadPlayers() {
+    let playersArray = localStorage.getItem('players').split(',');
+    playersArray.forEach(myFunction);
+}
+
+function myFunction(element, index) {
+    document.getElementsByClassName('players')[index].value = element;
 }
