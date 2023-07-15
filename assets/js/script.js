3e708f7ec63d4e9f6caf823a00c491aabe653f8a
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2023-07-15 18:53:30 +0100

    Add player names input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 function generatePlayers() {
     let limit = document.getElementById('playerCount').value;
     let players = document.getElementById('players');
-    console.log(players);
+    for (let i = 0 ; i < limit; i++) {
+        players.innerHTML += `<label for="player${i+1}">Player ${i+1}</label>
+        <input type="text" id="player${i+1}">`
+    }
+    console.log(players.innerHTML);
+
 }
